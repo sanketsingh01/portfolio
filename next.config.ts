b8325c79fd4f480eaad22443c84c9f0adad37218
Sanket Singh <vt118452@gmail.com>
2026-03-08 03:09:50 +0530

    fix/updated the next config file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  compress: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
